8e0d3fbdf6fba8677a29ea209dc7ca2ef113ba61
betogroo <luizhumberto@gmail.com>
2026-09-17 13:20:23 -0300

    feat: add profile menu with logout button

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,4 +1,12 @@
-<script setup></script>
+<script setup lang="ts">
+  const user = useSupabaseUser()
+  const supabase = useSupabaseClient()
+
+  const signOut = async () => {
+    await supabase.auth.signOut()
+    navigateTo('/login')
+  }
+</script>
 <template>
   <v-app app>
     <v-app-bar>
@@ -7,6 +15,23 @@
       <v-btn color="primary" to="/">Home</v-btn>
       <v-btn color="primary" to="/about">About</v-btn>
       <ThemeToggle />
+      <!-- Menu do usuário -->
+      <v-menu v-if="user">
+        <template #activator="{ props }">
+          <v-btn icon v-bind="props">
+            <v-icon>mdi-account</v-icon>
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item>
+            <v-list-item-title>{{ user.email }}</v-list-item-title>
+          </v-list-item>
+          <v-divider />
+          <v-list-item @click="signOut">
+            <v-list-item-title>Sair</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
     </v-app-bar>
     <v-main>
       <v-container>
