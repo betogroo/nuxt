eee2bb877383374ca24333bea2d8c23320d3afa0
betogroo <luizhumberto@gmail.com>
2026-09-17 13:31:05 -0300

    chore: configure devServer

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  devServer: {
+    host: '127.0.0.1',
+    port: 3000,
+  },
   alias: {
     cookie: 'cookie-es',
   },
