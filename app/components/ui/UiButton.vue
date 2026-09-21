4327a13e32669d6fc8fc928c8d10cab6cc47ca8c
betogroo <luizhumberto@gmail.com>
2026-09-21 13:29:20 -0300

    style(ui): refactor UiButton to act as a proper Vuetify 4 transparent wrapper

--- app/components/ui/UiButton.vue
+++ app/components/ui/UiButton.vue
@@ -1,28 +1,19 @@
 <script setup lang="ts">
   defineProps<{
     color?: string
-    variant?: 'elevated' | 'flat' | 'text' | 'outlined' | 'tonal'
-    loading?: boolean
-    disabled?: boolean
-    icon?: string
-    prependIcon?: string
-    block?: boolean
-    size?: string
+    variant?: 'elevated' | 'flat' | 'text' | 'outlined' | 'tonal' | 'plain'
+    icon?: string | boolean
   }>()
 </script>
 
 <template>
   <v-btn
-    :block="block"
     :color="color || 'primary'"
-    :disabled="disabled"
-    :icon="!!icon"
-    :loading="loading"
-    :prepend-icon="prependIcon"
-    :size="size"
+    :icon="icon"
     :variant="variant || (icon ? 'text' : 'elevated')"
   >
-    <v-icon v-if="icon">{{ icon }}</v-icon>
-    <slot v-else />
+    <template v-for="(_, name) in $slots" #[name]="slotData">
+      <slot :name="name" v-bind="slotData || {}" />
+    </template>
   </v-btn>
 </template>
